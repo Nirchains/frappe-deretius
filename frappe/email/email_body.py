b457770d998a64be8a5fc8c28e3d963623c20477
nirchains <nirchains@gmail.com>
2021-02-23 14:03:11 +0000

    Email body utf-8

--- frappe/email/email_body.py
+++ frappe/email/email_body.py
@@ -230,7 +230,7 @@ class EMail:
 	def make(self):
 		"""build into msg_root"""
 		headers = {
-			"Subject":        str(Header(strip(self.subject), "utf-8")),
+			"Subject":        Header(strip(self.subject), "utf-8").encode(),
 			"From":           self.sender,
 			"To":             ', '.join(self.recipients) if self.expose_recipients=="header" else "<!--recipient-->",
 			"Date":           email.utils.formatdate(),
